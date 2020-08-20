8ae1ef34d9966fc4ee907163ed9b308a741ef85a
badal sarkar <badalsarkar86@gmail.com>
2020-08-20 07:33:51 -0400

    Updated code

--- code/triangularNumber/Tester.java
+++ code/triangularNumber/Tester.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        System.out.println("The term in position "+n+" is :"+ TriangularNumber.getTerm(n));
+        System.out.print("Enter a desired term: ");
+        Scanner in = new Scanner(System.in);
+        int n =0;
+        try{
+            n = in.nextInt();
+            System.out.println("The term in position "+n+" is :"+ TriangularNumber.getTerm(n));
+        }
+        catch(NumberFormatException e){
+            System.out.println("Only integer number is accepted");
+        }
     }
 
 }
